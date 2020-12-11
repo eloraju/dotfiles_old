ee0f8a56f2ebbb91dce89aeb0bd5546ab227ac0d
Juuso Elo-Rauta <juuso.er@gmail.com>
2020-12-11 11:46:36 +0200

    minor changes and added rofi (might replace dmenu...)

--- .config/qtile/bar.py
+++ .config/qtile/bar.py
@@ -4,6 +4,7 @@ from libqtile import widget, bar
 from libqtile.config import Screen
 from libqtile.widget import net
 from shared import monitorCount, FONT, colors, bar_theme
+from socket import gethostname
 
 screens = []
 mons = monitorCount()
@@ -37,7 +38,7 @@ for mon in range(mons):
         pipeSpacer,
         widget.CurrentLayout(
             **bar_theme,
-            #scale = 0.6
+            # scale = 0.6
         ),
     ]
 
@@ -49,13 +50,6 @@ for mon in range(mons):
     ]
 
     right = [
-        widget.Wlan(
-            **bar_theme,
-            disconnected_message="No connection",
-            format="{essid}",
-            interface="wlp0s20f3"
-        ),
-        pipeSpacer,
         widget.CPU(
             **bar_theme,
             color_low=colors["med_green"],
@@ -73,7 +67,27 @@ for mon in range(mons):
         )
     ]
 
-    widgets = [
+    if gethostname() == "carbon":
+        right.insert(0, widget.Wlan(
+            **bar_theme,
+            disconnected_message="No connection",
+            format="{essid}",
+            interface="wlp0s20f3"
+            ))
+        right.insert(1, pipeSpacer)
+        right.extend([
+            pipeSpacer,
+            widget.Battery(
+            **bar_theme,
+            format="{char}{percent:2.0%} ({hour:d}:{min:02d})",
+            update_interval=60,
+            charge_char="+",
+            discharge_char="-",
+            notify_below=0.11
+            )])
+
+
+    widgets=[
         *left,
         widget.Spacer(**bar_theme),
         * middle,
